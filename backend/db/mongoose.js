1e753a6172a4d8531fd39a6dcb0f867e374e32bf
dawid12349 <47251508+dawid12349@users.noreply.github.com>
2021-03-13 08:45:56 +0100

    updated db

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const con = 'mongodb+srv://admin:skorpion1@cluster0-qpuuj.mongodb.net/tesla';
+const con = 'mongodb+srv://:@cluster0-qpuuj.mongodb.net/tesla';
 mongoose.connect(con, {
     useNewUrlParser: true,
     useCreateIndex: true,
